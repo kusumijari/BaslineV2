0ce0ca7f758d6fe92b6b9ea827d59eb028539868
kusumi <kusumi@INkusumi.local>
2015-08-28 17:59:37 +0530

    Created project skeleton

---
+++ src/Receipt.java
@@ -0,0 +1,5 @@
+/**
+ * Created by kusumi on 28/08/15.
+ */
+public class Receipt {
+}
---
+++ test/com/tw/salestax/ReceiptTest.java
@@ -0,0 +1,7 @@
+package com.tw.salestax;
+
+/**
+ * Created by kusumi on 28/08/15.
+ */
+public class ReceiptTest {
+}
